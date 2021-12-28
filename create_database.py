44b5abb1c485f15fa81675f8ae83f9d653ddc7af
flexiG17 <mister23krut@gmail.com>
2021-12-28 13:23:00 +0500

    clean columns skills and otherInfo

--- create_database.py
+++ create_database.py
@@ -1,8 +1,17 @@
 import sqlite3
 import pandas as pd
+import re
 
+
+def clean_field(field):
+    return re.sub(r'\<[^>]*\>', '', str(field))
+
+
+# подключаем базу данных, считываем данные с csv таблицы
 con = sqlite3.connect('database.sqlite')
 df = pd.read_csv("works.csv")
 cursor = con.cursor()
 
-
+# очищаем столбцы skills и otherInfo от ненужного
+df.skills = df.skills.apply(clean_field)
+df.otherInfo = df.otherInfo.apply(clean_field)
